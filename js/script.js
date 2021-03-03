fafcae344c9ff3d72e8aaa885ba2d21266425e0e
Filippo Garota <filogarota101@gmail.com>
2021-03-03 14:51:53 +0100

    Inizio esercizio

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*Lista Cognomi
+Chiedere all’utente il cognome
+inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+stampa la lista ordinata alfabeticamente
+scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova*/
+
+alert();
+
+// var cognomi = [‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’] ;
+// chiedo all'utente il cognome
